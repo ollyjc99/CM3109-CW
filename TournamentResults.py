4982ee1a44682c942f4298dee758fca75c7270eb
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2020-12-16 19:43:42 +0000

    more work on tournament class

--- TournamentResults.py
+++ TournamentResults.py
@@ -3,9 +3,23 @@ class TournamentResults:
         self.participants = participants
         self.matchups = dict()
 
+    def __str__(self):
+        return 'Tournament Results Class'
+
     def add_matchup(self, a, b, score):
         matchup = Matchup(a, b)
         self.matchups[matchup] = score
+
+    def get_matchup(self, a, b):
+        matchup = Matchup(a, b)
+        result = self.matchups.get(matchup)
+        if result:
+            return result
+        result = self.matchups.get(matchup.reverse())
+        if result:
+            return -result
+
+        return None
 
     def get_participant_name(self, participant):
         return self.participants[participant]
