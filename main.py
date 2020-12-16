401ac10b79d92492029c806e8ffedc107f65a640
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2020-12-16 22:22:45 +0000

    Testing Tournament Class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from TournamentResults import *
 from Ranking import *
 from SimulatedAnnealing import *
+from ParseResults import *
 import math
 from random import randint
 import argparse
@@ -8,16 +9,8 @@ import time
 
 
 def main(arg):
-    file = open(arg, 'r')
-
-    number_of_participants = int(file.readline())
-    participants = [[int(i), participant] for (i, participant) in [file.readline().strip().split(',') for _ in range(number_of_participants)]]
-
-    number_of_matchups = file.readline().split(',')[2]
-    print(number_of_matchups)
-
-    results = TournamentResults(participants)
-    print(results.get_participant_name(45))
+    results = parse_results.parse(arg)
+    print(results.get_participant_name(1))
 
     # Start of the algorithm
     time_start = time.perf_counter()
@@ -32,10 +25,9 @@ if __name__ == "__main__":
     TL = 10
     f = 0.95
     num_non_improve = 5000
-    try:
-        parser = argparse.ArgumentParser(description='Tournament Directed Graph')
-        parser.add_argument('results', type=str, help='Tournament Results')
-        args = parser.parse_args()
-        main(args.results)
-    except:
-        main('1994_Formula_One.wmg')
+
+    parser = argparse.ArgumentParser(description='Tournament Directed Graph')
+    parser.add_argument('results', type=str, help='Tournament Results')
+    args = parser.parse_args()
+    main(args.results)
+
