2dbd36a259a7f511e57253eac11990077d5b44b0
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2020-12-16 22:21:45 +0000

    returns an object containing all participants and matchups

--- ParseResults.py
+++ ParseResults.py
@@ -1,3 +1,22 @@
-class ParseResults:
-    def __init__(self):
-        pass
+from TournamentResults import *
+
+
+class parse_results(TournamentResults):
+        @staticmethod
+        def parse(filename):
+            file = open(filename, 'r')
+            number_of_participants = int(file.readline())
+            participants = [[int(i), participant] for (i, participant) in [file.readline().strip().split(',') for _ in range(number_of_participants)]]
+
+            results = TournamentResults(participants)
+            number_of_matchups = int(file.readline().split(',')[2])
+
+            for _ in range(number_of_matchups):
+                matchup = file.readline().strip().split(',')
+                participant_a = int(matchup[0])
+                participant_b = int(matchup[1])
+                score = int(matchup[2])
+                results.add_matchup(participant_a, participant_b, score)
+            file.close()
+
+            return results
