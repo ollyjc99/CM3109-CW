cb24b153a4a5127fd75b5d8fef720818695b967d
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2020-12-21 20:54:42 +0000

    Finished

--- main.py
+++ main.py
@@ -9,21 +9,31 @@ import time
 
 
 def main(arg):
-    results = parse_results.parse(arg)
-    print(results.get_participant_name(1))
-
+    results = ParseResults.parse(arg)
+    initial_ranking = Ranking(results, [i for i in range(1, 46 + 1)])
+    print('Average runtime ≈ 40.0(s) due to shamefully poor programming, please bear with ...')
     # Start of the algorithm
     time_start = time.perf_counter()
 
+    anneal_sim_model = SimulatedAnnealing()
+    ranking = anneal_sim_model.annealing_sim(initial_ranking, ti, tl, f, num_non_improve)
+
     # End of the algorithm
     time_finish = time.perf_counter()
+    print(f'Best ranking: {ranking.ranking}')
+    print(f'Kemeny Score: {ranking.get_kemeny_score()}')
+    print(f'Uphill moves: {anneal_sim_model.get_uphill_moves()}')
     print(f'Finished in {round(time_finish - time_start, 3)} second(s)')
 
 
 if __name__ == "__main__":
-    TI = 1.0
-    TL = 10
-    f = 0.95
+    # Initial Temperature
+    ti = 1.0
+    # Temperature Length
+    tl = 10
+    # Cooling Ratio
+    f = .75
+    # Stopping Criterion
     num_non_improve = 5000
 
     parser = argparse.ArgumentParser(description='Tournament Directed Graph')
